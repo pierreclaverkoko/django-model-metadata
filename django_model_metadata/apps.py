88555926a1ca2dc1aacb49a77c14e3d825471854
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2025-03-12 12:12:48 +0200

    Added pre-commit

--- django_model_metadata/apps.py
+++ django_model_metadata/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MetadataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'metadata'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "metadata"
