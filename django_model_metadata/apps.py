304e4cbb8acf6613608c3e50a1c3be2d310cfd12
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2025-03-13 11:10:04 +0200

    Fixing bug

--- django_model_metadata/apps.py
+++ django_model_metadata/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MetadataConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "metadata"
+    name = "django_model_metadata"
