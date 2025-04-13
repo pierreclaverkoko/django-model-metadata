30d77ff5b58b6bb670bf9f7a58c42a78ed2b4016
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2025-04-13 16:05:35 +0200

    Changes for models

--- django_model_metadata/models.py
+++ django_model_metadata/models.py
@@ -1,5 +1,6 @@
-from .model_mixins import GeneralMetadataMixin
+from django.db import models
 import swapper
+from .model_mixins import GeneralMetadataMixin
 
 
 class ModelGeneralMetaData(GeneralMetadataMixin):
@@ -7,6 +8,9 @@ class ModelGeneralMetaData(GeneralMetadataMixin):
     Metadata model
     For general use
     """
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         swappable = swapper.swappable_setting("django_model_metadata", "ModelGeneralMetaData")
